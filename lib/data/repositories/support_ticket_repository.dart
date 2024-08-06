11476d5b29aa38f826c5ed1d8e285a5e84768813
obada kraishan <obada.kraishan@gmail.com>
2024-08-06 13:38:27 -0500

    Implement Customer Support

---
+++ lib/data/repositories/support_ticket_repository.dart
@@ -0,0 +1,26 @@
+import 'package:grocer_ease/data/models/support_ticket_model.dart';
+import 'package:grocer_ease/services/support_ticket_service.dart';
+
+class SupportTicketRepository {
+  final SupportTicketService _supportTicketService = SupportTicketService();
+
+  Future<void> createSupportTicket(SupportTicketModel ticket) async {
+    await _supportTicketService.createSupportTicket(ticket);
+  }
+
+  Future<SupportTicketModel?> getSupportTicket(String ticketId) async {
+    return await _supportTicketService.getSupportTicket(ticketId);
+  }
+
+  Future<void> updateSupportTicket(SupportTicketModel ticket) async {
+    await _supportTicketService.updateSupportTicket(ticket);
+  }
+
+  Future<void> deleteSupportTicket(String ticketId) async {
+    await _supportTicketService.deleteSupportTicket(ticketId);
+  }
+
+  Future<List<SupportTicketModel>> getAllSupportTicketsForUser(String userId) async {
+    return await _supportTicketService.getAllSupportTicketsForUser(userId);
+  }
+}
