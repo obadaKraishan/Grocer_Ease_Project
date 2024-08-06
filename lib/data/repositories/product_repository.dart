07b1a05c43e0a44ed54c04f1992d373e750392e2
obada kraishan <obada.kraishan@gmail.com>
2024-08-06 12:43:27 -0500

    Implement Product Management

---
+++ lib/data/repositories/product_repository.dart
@@ -0,0 +1,26 @@
+import 'package:grocer_ease/data/models/product_model.dart';
+import 'package:grocer_ease/services/product_service.dart';
+
+class ProductRepository {
+  final ProductService _productService = ProductService();
+
+  Future<void> createProduct(ProductModel product) async {
+    await _productService.createProduct(product);
+  }
+
+  Future<ProductModel?> getProduct(String productId) async {
+    return await _productService.getProduct(productId);
+  }
+
+  Future<void> updateProduct(ProductModel product) async {
+    await _productService.updateProduct(product);
+  }
+
+  Future<void> deleteProduct(String productId) async {
+    await _productService.deleteProduct(productId);
+  }
+
+  Future<List<ProductModel>> getAllProducts() async {
+    return await _productService.getAllProducts();
+  }
+}
